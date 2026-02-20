6552545b71f1eda80e960a615f4fccdb23566732
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-20 21:23:45 +0800

    feat(entry): unify public exports and load DOM event augmentation

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Load global HTMLElementEventMap augmentation for cropper events (addEventListener typing).
+import './cropper/types/dom-events'
+
+import VuePictureCropper from './cropper/index.vue'
+
+export default VuePictureCropper
+
+export * from './cropper/types'
+export * from './hooks/useCropper'
