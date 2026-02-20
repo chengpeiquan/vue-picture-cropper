5f64f41052efd13021d2ce94fdde9f677429c6de
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-20 21:28:07 +0800

    chore(build): replace Vite with tsdown as build tool

---
+++ tsdown.config.ts
@@ -0,0 +1,27 @@
+import { getBundleBanner } from '@bassist/build-config/tsup'
+import { defineConfig } from 'tsdown'
+import Vue from 'unplugin-vue/rolldown'
+import pkg from './package.json' with { type: 'json' }
+
+export default defineConfig({
+  entry: {
+    index: './src/index.ts',
+  },
+  platform: 'browser',
+  exports: true,
+  fixedExtension: true,
+  banner: getBundleBanner(pkg),
+  minify: true,
+  exports: true,
+  dts: {
+    vue: true,
+  },
+  css: {
+    splitting: false,
+  },
+  plugins: [
+    Vue({
+      isProduction: true,
+    }),
+  ],
+})
