ff01392eb6dda8531dc18ec0fe064fb5abcc8d1a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-22 02:55:06 +0800

    feat(docs): add CodeFragments, CodeTemplate, and SourceCode components for rendering code snippets

---
+++ docs/components/code-widgets/CodeTemplate.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { cn } from '../../utils/class-name'
+import { type SourceCodeProps } from './types'
+
+defineProps<SourceCodeProps>()
+</script>
+
+<template>
+  <!-- VitePress code block style -->
+  <div v-if="code" :class="cn(`language-${lang}`, className)">
+    <button title="Copy Code" class="copy" />
+    <span class="lang">{{ lang }}</span>
+    <pre
+      class="shiki shiki-themes github-light github-dark"
+      style="
+        --shiki-light: #24292e;
+        --shiki-dark: #e1e4e8;
+        --shiki-light-bg: #fff;
+        --shiki-dark-bg: #24292e;
+      "
+      tabindex="0"
+      dir="ltr"
+      v-html="code"
+    />
+  </div>
+</template>
