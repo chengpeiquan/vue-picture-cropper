8cd6fa3bef75bf00d5afbe63b388274b33778534
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-22 02:55:43 +0800

    feat(docs): add CropAndPreview component for displaying cropped images and previews

---
+++ docs/components/CropAndPreview.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { cn } from '../utils/class-name'
+
+defineProps<{
+  croppedPreview: string
+}>()
+
+const blockClassName = cn(
+  'flex min-w-0 flex-1 flex-col gap-4',
+  'border border-[var(--p-tabs-tablist-border-color)]',
+  'box-border rounded-md p-4',
+)
+
+const titleClassName = cn('!m-0 text-sm font-bold text-primary')
+
+const contentClassName = cn('aspect-[4/3] w-full overflow-hidden rounded-md')
+</script>
+
+<template>
+  <div class="flex w-full flex-col gap-4 sm:flex-row">
+    <div :class="blockClassName">
+      <p :class="titleClassName">
+        {{ $t('examples.cropArea') }}
+      </p>
+
+      <div :class="contentClassName">
+        <slot />
+      </div>
+    </div>
+
+    <div :class="blockClassName">
+      <p :class="titleClassName">
+        {{ $t('examples.previewArea') }}
+      </p>
+
+      <div :class="cn(contentClassName, 'bg-zinc-100 dark:bg-zinc-800')">
+        <img
+          v-if="croppedPreview"
+          :src="croppedPreview"
+          alt="Crop preview"
+          class="size-full object-contain"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+x
