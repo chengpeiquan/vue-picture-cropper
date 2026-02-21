654fb176aab9cd8d1b51afc55f1f5ba1b3174dd7
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-22 02:56:38 +0800

    feat(docs): add ChooseFile component for file selection with image upload functionality

---
+++ docs/components/ChooseFile.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { FileUpload } from 'primevue'
+
+defineProps<{
+  onSelect: (file: File) => void
+}>()
+</script>
+
+<template>
+  <FileUpload
+    mode="basic"
+    custom-upload
+    auto
+    :choose-label="$t('examples.choose')"
+    accept="image/*"
+    severity="secondary"
+    @select="
+      (e) => {
+        const selectedFile = e.files[0]
+        onSelect(selectedFile)
+      }
+    "
+  />
+</template>
