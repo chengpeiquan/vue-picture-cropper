4795485bfcef86bbdab9e3aa8d7af6fbedd8c080
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-02-24 01:05:47 +0800

    feat(docs): enhance NpmVersionBadge component with additional badges and improve layout

--- docs/components/NpmVersionBadge.vue
+++ docs/components/NpmVersionBadge.vue
@@ -1,15 +1,33 @@
-<script setup lang="ts">
-import { cn } from '../utils/class-name'
-</script>
-
 <template>
-  <a
-    :class="cn('block min-h-5')"
-    href="https://www.npmjs.com/package/vue-picture-cropper"
-  >
-    <img
-      src="https://img.shields.io/npm/v/vue-picture-cropper?color=3fb984&label=npm"
-      alt="npm version"
-    />
-  </a>
+  <div class="flex min-h-5 shrink-0 items-center gap-2">
+    <a
+      href="https://www.npmjs.com/package/vue-picture-cropper"
+      target="__blank"
+    >
+      <img
+        src="https://img.shields.io/npm/v/vue-picture-cropper?color=3fb984&label=npm"
+        alt="npm version"
+      />
+    </a>
+
+    <a
+      href="https://www.npmjs.com/package/vue-picture-cropper"
+      target="__blank"
+    >
+      <img
+        src="https://img.shields.io/npm/dy/vue-picture-cropper?color=3fb984&label=downloads"
+        alt="downloads"
+      />
+    </a>
+
+    <a
+      href="https://github.com/chengpeiquan/vue-picture-cropper"
+      target="__blank"
+    >
+      <img
+        src="https://img.shields.io/github/stars/chengpeiquan/vue-picture-cropper?style=social"
+        alt="GitHub stars"
+      />
+    </a>
+  </div>
 </template>
